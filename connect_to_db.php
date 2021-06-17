b2f6ddec0312dd65a2dbccd8cd9fe6c5f412a9ec
ItzKartik <smartzboi@gmail.com>
2021-06-17 19:48:52 +0530

    work

--- connect_to_db.php
+++ connect_to_db.php
@@ -2,7 +2,7 @@
 
 $server = "localhost";
 $username = "root";
-$password = "";
+$password = "hari47";
 $con = mysqli_connect($server, $username, $password);
 if(!$con){
     die("connection failed". mysqli_error());
